074cc558beb962227f58b1caa3bba2132ba4f3a4
James Aprosail <aprosail@outlook.com>
2024-06-28 17:26:48 +0800

    rename into maparea

---
+++ lib/maparea.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+import 'package:modifier/modifier.dart';
+
+class MapArea extends StatefulWidget {
+  const MapArea({super.key});
+
+  @override
+  State<MapArea> createState() => _MapAreaState();
+}
+
+class _MapAreaState extends State<MapArea> {
+  @override
+  Widget build(BuildContext context) => const Text('map area').center;
+}
